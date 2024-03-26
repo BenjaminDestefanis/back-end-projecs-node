9fcc2062bdaac3e8c6a87c54148ac5a3f15e5336
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-03-26 17:43:24 -0300

    26/03/24 5:43 PM

--- OS_info/index.js
+++ OS_info/index.js
@@ -1,17 +1,28 @@
 const os = require("os")
 const fs = require("fs")
 
+// Seconds to Minutes to Houers
+
+const timeConverter = (seconds) => {
+    let hour = Math.floor(seconds / 3600)
+    hour = (hour < 10) ? '0' + hour : hour
+    let minute = Math.floor((seconds / 60) % 60)
+    minute = (minute < 10) ? '0' + minute : minute
+    let second = Math.floor(seconds % 60)
+    second = (second < 10) ? '0' + second : second
+
+    return `${hour}:${minute}:${second}`
+}
+
 const platform = os.platform()
 const type = os.type()
 const arch = os.arch()
 const release = os.release()
-const upTime = os.uptime()
+const upTime = timeConverter(os.uptime())
 const totalMem = os.totalmem()
 const freeMem = os.freemem()
 
-const osInfo = `Plataroma : ${platform} \n
-                Typo : ${type} \n
-                `
+const osInfo = `Plataforma : ${platform} \nTypo : ${type} \nArquitectura : ${arch} \nVersion : ${release} \nTiempo Encendido : ${upTime} \nRAM : ${totalMem} \nRAM libre : ${freeMem}}`
 
 
 const createDocument = (document, infoDocument) => {
@@ -20,11 +31,10 @@ const createDocument = (document, infoDocument) => {
             console.error(err)
             return
         } else {
-            console.log(`OS info created sucess [${infoDocument}]`)
+            console.log(`OS info created sucess : \n${infoDocument}`)
         }
     })
 }
 
-let myArr = [1,2,3,4,5]
 
-//createDocument('info-OS.txt', platform)
+createDocument('info-OS.txt', osInfo)
