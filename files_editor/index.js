09f5a0ede8e00be98654769be8b03d94bad4e964
= <benjaminisraelb@gmail.com>
2024-03-26 00:26:40 -0300

    26/03/24 00:26 AM

--- files_editor/index.js
+++ files_editor/index.js
@@ -1,4 +1,24 @@
 const fs = require("fs")
 
-console.log(fs)
-console.log("TEST")
+const createFile = (fileName, content) => {
+    fs.writeFile(fileName, content, (err) => {
+        if(err){
+            console.error(err)
+            return
+        } else {
+            console.log(`Archivo ${fileName} a sido creado con exitosamente`)
+        }
+    })
+}
+
+const readingFile = (fileName) => {
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+        if (err){
+            console.error(err)
+            return
+        } else {
+            console.log(`Info of archive ${fileName}: \n ${data}`)
+        }
+    })
+}
+
