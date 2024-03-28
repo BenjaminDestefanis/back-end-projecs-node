301ec0051375e37782dba8bebbaa08ddf585596c
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-03-28 12:23:34 -0300

    3/28/24 12:23PM

---
+++ API-Task-list/db-connect.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { DB_USER, DB_PASSWORD } = process.env;
+const mongoose = require('mongoose');
+const colors = require('colors')
+
+const dbUri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@tasklistdb.omwu7qi.mongodb.net/`;
+
+const mongoConnect = async () => {
+    try{
+        await mongoose.connect(dbUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+    })
+    
+    console.log('Conexión exitosa a la base de datos.'.green)
+  
+} catch(error) {
+    console.log('Error de conexión a la base de datos:'.red , error)
+}};
+
+mongoConnect()
+
+const db = mongoose.connection;
+
+
+module.exports = db;
---
+++ API-Task-list/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+const db = require('./db-connect')
+
+
+
+
+const app = express()
+
+
+
+
