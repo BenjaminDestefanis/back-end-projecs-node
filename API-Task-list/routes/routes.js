296504b8bcd4ce12a204de3670bb805e876d9ca8
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-03-28 18:03:15 -0300

    3/28/24 6:03PM

--- API-Task-list/routes/routes.js
+++ API-Task-list/routes/routes.js
@@ -4,13 +4,17 @@ const Task = require('../models/models')
 const router = express.Router()
 const mongoose = require("mongoose")
 
+//app.use(express.json())
+app.use(express.urlencoded({ extended : true}))
+
 router.get('/tasks', async (req, res) => {
     const tasks = await Task.find()
     res.json(tasks)
 })
 
-router.post('/createTask', async (req, res) => {
-    res.send("POST")
+router.post('/createTask',  (req, res) => {
+    //const taskName = req.body.task_name
+    console.log(req.body.description)
     /* const task = new Task(req.body)
     await task.save()
     res.json(task) */
